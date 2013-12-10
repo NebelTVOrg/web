54635713e9e380bd5e76f7468713e563cd1c88d1
Aleksey Baidan <howtwizer@gmail.com>
2013-12-10 16:36:00 +0200

    social links

---
+++ wp-content/themes/nebel.tv/inc/social.php
@@ -0,0 +1,8 @@
+
+<ul class="social-links">
+     <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social-icons/linkedin.png" alt="LinkedIn" width="33" height="39" /></a></li>
+     <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social-icons/facebook.png" alt="Facebook" width="33" height="39" /></a></li>
+     <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social-icons/twitter.png" alt="Twitter" width="33" height="39" /></a></li>
+     <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social-icons/gplus.png" alt="GooglePlus" width="33" height="39" /></a></li>
+     <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social-icons/youtube.png" alt="Youtube" width="33" height="39" /></a></li>
+ </ul>
