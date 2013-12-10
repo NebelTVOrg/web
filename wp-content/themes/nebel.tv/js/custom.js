9213b6e852a3313d8113369a14a52f56f27f7fe2
Aleksey Baidan <howtwizer@gmail.com>
2013-12-10 16:10:14 +0200

    blog page

--- wp-content/themes/nebel.tv/js/custom.js
+++ wp-content/themes/nebel.tv/js/custom.js
@@ -20,7 +20,13 @@
     });
     /* EOF Side menu */
 
-
+    var win_h = $(window).height();
+    var sh_h = $('.site-header').height();
+    var sb_h = $('.blog-sidebar').height();
+    /*  if(0 != sb_h) {
+     $('.content-scroll').height(sb_h - 100);
+     } */
+    $('.content-scroll').height(win_h - sh_h - (0.14 * win_h ));
 
 
 } )( jQuery );
