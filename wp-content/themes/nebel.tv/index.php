ec27c5d84d10b623fc663638b0392acb6732820f
Alexey Galkevych <evpatij76@gmail.com>
2013-12-10 20:51:35 +0200

    Fixed error

--- wp-content/themes/nebel.tv/index.php
+++ wp-content/themes/nebel.tv/index.php
@@ -16,28 +16,28 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area clear">
-    <div id="content" class="site-content">
-        <article class="blog">
-            <div class="entry-holder content-scroll">
-                 <div class="entry-content">
-                    <ul class="posts">
-                        <?php while ( have_posts() ) : the_post(); ?>
-                            <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                                <?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
-                                    <div class="entry-thumbnail">
-                                        <?php the_post_thumbnail('thumb'); ?>
-                                    </div>
-                                <?php endif; ?>
-                                <div class="post-preview">
-                                    <header class="entry-header clear">
-                                        <?php aeg_author_meta(); ?>
-                                         <h2 class="post-title"><a class="greener" href="<?php echo get_permalink(get_the_ID())?>"><?php the_title(); ?></a></h2>                                         
-                                   </header>
+    <div id="primary" class="content-area clear">
+        <div id="content" class="site-content" role="main">
+            <article class="blog">
+                <div class="entry-holder content-scroll">
+                    <div class="entry-content">
+                        <ul class="posts">
+                            <?php while ( have_posts() ) : the_post(); ?>
+                                <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                                    <?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+                                        <div class="entry-thumbnail">
+                                            <?php the_post_thumbnail('thumb'); ?>
+                                        </div>
+                                    <?php endif; ?>
+                                    <div class="post-preview">
+                                        <header class="entry-header clear">
+                                            <?php aeg_author_meta(); ?>
+                                            <h2 class="post-title"><a class="greener" href="<?php echo get_permalink(get_the_ID())?>"><?php the_title(); ?></a></h2>
+                                        </header>
                                         <div class="entry-summary">
-                                            <?php 
-                                                $excerpt = get_the_excerpt(); 
-                                                echo aeg_string_limit_words($excerpt, 30);
+                                            <?php
+                                            $excerpt = get_the_excerpt();
+                                            echo aeg_string_limit_words($excerpt, 30);
                                             ?>
                                         </div>
                                         <div class="entry-meta">
@@ -47,20 +47,20 @@ get_header(); ?>
                                                 <?php aeg_post_date(); ?>
                                                 <?php aeg_tag_meta(); ?>
                                                 <?php aeg_cat_meta(); ?>
-                                            </div>                                                
+                                            </div>
                                         </div>
-                                </div><!-- .post-preview -->
-                            </li><!-- #post -->
-                        <?php endwhile; ?>
-                    </ul>
+                                    </div><!-- .post-preview -->
+                                </li><!-- #post -->
+                            <?php endwhile; ?>
+                        </ul>
+                    </div>
                 </div>
+            </article>
+            <div class="sub-content clear">
+                <?php aeg_pagination(); ?>
+                <?php get_template_part('inc/social'); ?>
             </div>
-        </article>
-        <div class="sub-content clear">
-            <?php aeg_pagination(); ?>
-            <?php get_template_part('inc/social'); ?>
-        </div>
-    ><!-- #content -->
-    <?php get_sidebar(); ?>
-</div><!-- #primary -->
+        </div><!-- #content -->
+        <?php get_sidebar(); ?>
+    </div><!-- #primary -->
 <?php get_footer(); ?>
