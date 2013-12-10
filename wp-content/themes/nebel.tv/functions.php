c32d4dcc74cd519351583349077ef0a73f489717
Aleksey Baidan <howtwizer@gmail.com>
2013-12-10 16:21:14 +0200

    Blog functions

--- wp-content/themes/nebel.tv/functions.php
+++ wp-content/themes/nebel.tv/functions.php
@@ -104,7 +104,8 @@ function twentythirteen_setup() {
 	 * "standard" posts and pages.
 	 */
 	add_theme_support( 'post-thumbnails' );
-	set_post_thumbnail_size( 604, 270, true );
+    set_post_thumbnail_size( 300, 9999 );
+    add_image_size('thumb', 112, 112, true);
 
 	// This theme uses its own gallery styles.
 	add_filter( 'use_default_gallery_style', '__return_false' );
@@ -276,60 +277,60 @@ if ( ! function_exists( 'twentythirteen_paging_nav' ) ) :
  *
  * @return void
  */
-function twentythirteen_paging_nav() {
-	global $wp_query;
+    function twentythirteen_paging_nav() {
+        global $wp_query;
 
-	// Don't print empty markup if there's only one page.
-	if ( $wp_query->max_num_pages < 2 )
-		return;
-	?>
-	<nav class="navigation paging-navigation" role="navigation">
-		<h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'twentythirteen' ); ?></h1>
-		<div class="nav-links">
+        // Don't print empty markup if there's only one page.
+        if ( $wp_query->max_num_pages < 2 )
+            return;
+        ?>
+        <nav class="navigation paging-navigation" role="navigation">
+            <h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'twentythirteen' ); ?></h1>
+            <div class="nav-links">
 
-			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentythirteen' ) ); ?></div>
-			<?php endif; ?>
+                <?php if ( get_next_posts_link() ) : ?>
+                    <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentythirteen' ) ); ?></div>
+                <?php endif; ?>
 
-			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentythirteen' ) ); ?></div>
-			<?php endif; ?>
+                <?php if ( get_previous_posts_link() ) : ?>
+                    <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentythirteen' ) ); ?></div>
+                <?php endif; ?>
 
-		</div><!-- .nav-links -->
-	</nav><!-- .navigation -->
-	<?php
-}
+            </div><!-- .nav-links -->
+        </nav><!-- .navigation -->
+    <?php
+    }
 endif;
 
 if ( ! function_exists( 'twentythirteen_post_nav' ) ) :
-/**
- * Display navigation to next/previous post when applicable.
-*
-* @since nebel.tv 1.0
-*
-* @return void
-*/
-function twentythirteen_post_nav() {
-	global $post;
+    /**
+     * Display navigation to next/previous post when applicable.
+     *
+     * @since nebel.tv 1.0
+     *
+     * @return void
+     */
+    function twentythirteen_post_nav($limit) {
+        global $post;
 
-	// Don't print empty markup if there's nowhere to navigate.
-	$previous = ( is_attachment() ) ? get_post( $post->post_parent ) : get_adjacent_post( false, '', true );
-	$next     = get_adjacent_post( false, '', false );
+        // Don't print empty markup if there's nowhere to navigate.
+        $previous = ( is_attachment() ) ? get_post( $post->post_parent ) : get_adjacent_post( false, '', true );
+        $next     = get_adjacent_post( false, '', false );
 
-	if ( ! $next && ! $previous )
-		return;
-	?>
-	<nav class="navigation post-navigation" role="navigation">
-		<h1 class="screen-reader-text"><?php _e( 'Post navigation', 'twentythirteen' ); ?></h1>
-		<div class="nav-links">
+        if ( ! $next && ! $previous )
+            return;
+        ?>
+        <nav class="navigation post-navigation" role="navigation">
+            <h1 class="screen-reader-text"><?php _e( 'Post navigation', 'twentythirteen' ); ?></h1>
+            <div class="nav-links clear">
 
-			<?php previous_post_link( '%link', _x( '<span class="meta-nav">&larr;</span> %title', 'Previous post link', 'twentythirteen' ) ); ?>
-			<?php next_post_link( '%link', _x( '%title <span class="meta-nav">&rarr;</span>', 'Next post link', 'twentythirteen' ) ); ?>
+                <span class="meta-nav-prev"><?php previous_post_link( '%link', _x( '&laquo; Previous post', 'Previous post link', 'twentythirteen' ) ); ?></span>
+                <span class="meta-nav-next"><?php next_post_link( '%link', _x( 'Next post &raquo;', 'Next post link', 'twentythirteen' ) ); ?></span>
 
-		</div><!-- .nav-links -->
-	</nav><!-- .navigation -->
-	<?php
-}
+            </div><!-- .nav-links -->
+        </nav><!-- .navigation -->
+    <?php
+    }
 endif;
 
 if ( ! function_exists( 'twentythirteen_entry_meta' ) ) :
@@ -562,4 +563,280 @@ function twentythirteen_customize_preview_js() {
 	wp_enqueue_script( 'twentythirteen-customizer', get_template_directory_uri() . '/js/theme-customizer.js', array( 'customize-preview' ), '20130226', true );
 }
 add_action( 'customize_preview_init', 'twentythirteen_customize_preview_js' );
+
+// The excerpt based on words
+
+function aeg_string_limit_words($string, $word_limit)
+{
+    $words = explode(' ', $string, ($word_limit + 1));
+    if(count($words) > $word_limit)
+        array_pop($words);
+    return implode(' ', $words).'... ';
+}
+function aeg_author_meta() {
+    printf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">by <strong>%3$s</strong></a></span>',
+        esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+        esc_attr( sprintf( __( 'View all posts by %s', 'nebel.tv' ), get_the_author() ) ),
+        get_the_author()
+    );
+}
+function aeg_tag_meta() {
+    $tag_list = get_the_tag_list( '', __( ', ', 'nebel.tv' ) );
+    if ( $tag_list ) {
+        echo '<span class="tags-links">Tags: ' . $tag_list . '</span>';
+    }
+}
+function aeg_cat_meta() {
+    $categories_list = get_the_category_list( __( ' ', 'nebel.tv' ) );
+    if ( $categories_list ) {
+        echo '<span class="categories-links">' . $categories_list . '</span>';
+    }
+}
+function aeg_post_date( $echo = true ) {
+
+    $date = sprintf( '<time datetime="%s">
+                        <span>%s</span>
+                      </time>',
+        esc_attr(get_the_date('Y-m-d')),
+        esc_attr(get_the_date('j.m.Y'))
+    );
+
+    if ( $echo )
+        echo $date;
+
+    return $date;
+}
+// Pagination
+
+function aeg_pagination($pages = '', $range = 1)
+{
+    $showitems = ($range * 2)+1;
+
+    global $paged;
+    if(empty($paged)) $paged = 1;
+
+    if($pages == '')
+    {
+        global $wp_query;
+        $pages = $wp_query->max_num_pages;
+        if(!$pages)
+        {
+            $pages = 1;
+        }
+    }
+
+    if(1 != $pages)
+    {
+        echo "<ul class=\"pagination\">";
+        //if($paged > 2 && $paged > $range+1 && $showitems < $pages) echo "<li class='first'><a href='".get_pagenum_link(1)."'>First</a></li>";
+        if($paged > 1) echo "<li class='prev'><a href='".get_pagenum_link($paged - 1)."'>Prev</a></li>";
+
+        for ($i=1; $i <= $pages; $i++)
+        {
+            if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems ))
+            {
+                echo ($paged == $i)? "<li class=\"active green-gr\"><a href=''>".$i."</a></li>":"<li class=\"grey-gr\"><a href='".get_pagenum_link($i)."' class=\"inactive\">".$i."</a></li>";
+            }
+        }
+
+        if ($paged < $pages) echo "<li class='next'><a href=\"".get_pagenum_link($paged + 1)."\">Next</a></li>";
+        //if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) echo "<li class='last'><a href='".get_pagenum_link($pages)."'>Last</a></li>";
+        echo "</ul>\n";
+    }
+}
+
+// Calendar
+
+function aeg_get_calendar($initial = true, $echo = true) {
+    global $wpdb, $m, $monthnum, $year, $wp_locale, $posts;
+
+    $cache = array();
+    $key = md5( $m . $monthnum . $year );
+    if ( $cache = wp_cache_get( 'aeg_get_calendar', 'calendar' ) ) {
+        if ( is_array($cache) && isset( $cache[ $key ] ) ) {
+            if ( $echo ) {
+                echo apply_filters( 'aeg_get_calendar',  $cache[$key] );
+                return;
+            } else {
+                return apply_filters( 'aeg_get_calendar',  $cache[$key] );
+            }
+        }
+    }
+
+    if ( !is_array($cache) )
+        $cache = array();
+
+    // Quick check. If we have no posts at all, abort!
+    if ( !$posts ) {
+        $gotsome = $wpdb->get_var("SELECT 1 as test FROM $wpdb->posts WHERE post_type = 'post' AND post_status = 'publish' LIMIT 1");
+        if ( !$gotsome ) {
+            $cache[ $key ] = '';
+            wp_cache_set( 'aeg_get_calendar', $cache, 'calendar' );
+            return;
+        }
+    }
+
+    if ( isset($_GET['w']) )
+        $w = ''.intval($_GET['w']);
+
+    // week_begins = 0 stands for Sunday
+    $week_begins = intval(get_option('start_of_week'));
+
+    // Let's figure out when we are
+    if ( !empty($monthnum) && !empty($year) ) {
+        $thismonth = ''.zeroise(intval($monthnum), 2);
+        $thisyear = ''.intval($year);
+    } elseif ( !empty($w) ) {
+        // We need to get the month from MySQL
+        $thisyear = ''.intval(substr($m, 0, 4));
+        $d = (($w - 1) * 7) + 6; //it seems MySQL's weeks disagree with PHP's
+        $thismonth = $wpdb->get_var("SELECT DATE_FORMAT((DATE_ADD('{$thisyear}0101', INTERVAL $d DAY) ), '%m')");
+    } elseif ( !empty($m) ) {
+        $thisyear = ''.intval(substr($m, 0, 4));
+        if ( strlen($m) < 6 )
+            $thismonth = '01';
+        else
+            $thismonth = ''.zeroise(intval(substr($m, 4, 2)), 2);
+    } else {
+        $thisyear = gmdate('Y', current_time('timestamp'));
+        $thismonth = gmdate('m', current_time('timestamp'));
+    }
+
+    $unixmonth = mktime(0, 0 , 0, $thismonth, 1, $thisyear);
+    $last_day = date('t', $unixmonth);
+
+    // Get the next and previous month and year with at least one post
+    $previous = $wpdb->get_row("SELECT MONTH(post_date) AS month, YEAR(post_date) AS year
+		FROM $wpdb->posts
+		WHERE post_date < '$thisyear-$thismonth-01'
+		AND post_type = 'post' AND post_status = 'publish'
+			ORDER BY post_date DESC
+			LIMIT 1");
+    $next = $wpdb->get_row("SELECT MONTH(post_date) AS month, YEAR(post_date) AS year
+		FROM $wpdb->posts
+		WHERE post_date > '$thisyear-$thismonth-{$last_day} 23:59:59'
+		AND post_type = 'post' AND post_status = 'publish'
+			ORDER BY post_date ASC
+			LIMIT 1");
+
+    /* translators: Calendar caption: 1: month name, 2: 4-digit year */
+    $calendar_caption = _x('%1$s %2$s', 'calendar caption');
+    $calendar_output = '<table id="wp-calendar">
+	<caption>';
+    if ( $previous ) {
+        $calendar_output .= '<span id="prev"><a href="' . get_month_link($previous->year, $previous->month) . '" title="' . esc_attr( sprintf(__('View posts for %1$s %2$s'), $wp_locale->get_month($previous->month), date('Y', mktime(0, 0 , 0, $previous->month, 1, $previous->year)))) . '">&lt;</a></span>';
+    } else {
+        $calendar_output .= '<span id="prev" class="pad">&nbsp;</span>';
+    }
+
+    $calendar_output .= sprintf($calendar_caption, $wp_locale->get_month($thismonth), date('Y', $unixmonth));
+    if ( $next ) {
+        $calendar_output .= '<span id="next"><a href="' . get_month_link($next->year, $next->month) . '" title="' . esc_attr( sprintf(__('View posts for %1$s %2$s'), $wp_locale->get_month($next->month), date('Y', mktime(0, 0 , 0, $next->month, 1, $next->year))) ) . '">&gt;</a></span>';
+    } else {
+        $calendar_output .= '<span id="next" class="pad">&nbsp;</span>';
+    }
+    $calendar_output .= '</caption>
+	<thead>
+	<tr>';
+
+    $myweek = array();
+
+    for ( $wdcount=0; $wdcount<=6; $wdcount++ ) {
+        $myweek[] = $wp_locale->get_weekday(($wdcount+$week_begins)%7);
+    }
+
+    foreach ( $myweek as $wd ) {
+        $day_name = (true == $initial) ? $wp_locale->get_weekday_initial($wd) : $wp_locale->get_weekday_abbrev($wd);
+        $wd = esc_attr($wd);
+        $calendar_output .= "\n\t\t<th scope=\"col\" title=\"$wd\">$day_name</th>";
+    }
+
+    $calendar_output .= '
+	</tr>
+	</thead>';
+
+    $calendar_output .= '
+
+	<tbody>
+	<tr>';
+
+    // Get days with posts
+    $dayswithposts = $wpdb->get_results("SELECT DISTINCT DAYOFMONTH(post_date)
+		FROM $wpdb->posts WHERE post_date >= '{$thisyear}-{$thismonth}-01 00:00:00'
+		AND post_type = 'post' AND post_status = 'publish'
+		AND post_date <= '{$thisyear}-{$thismonth}-{$last_day} 23:59:59'", ARRAY_N);
+    if ( $dayswithposts ) {
+        foreach ( (array) $dayswithposts as $daywith ) {
+            $daywithpost[] = $daywith[0];
+        }
+    } else {
+        $daywithpost = array();
+    }
+
+    if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false || stripos($_SERVER['HTTP_USER_AGENT'], 'camino') !== false || stripos($_SERVER['HTTP_USER_AGENT'], 'safari') !== false)
+        $ak_title_separator = "\n";
+    else
+        $ak_title_separator = ', ';
+
+    $ak_titles_for_day = array();
+    $ak_post_titles = $wpdb->get_results("SELECT ID, post_title, DAYOFMONTH(post_date) as dom "
+        ."FROM $wpdb->posts "
+        ."WHERE post_date >= '{$thisyear}-{$thismonth}-01 00:00:00' "
+        ."AND post_date <= '{$thisyear}-{$thismonth}-{$last_day} 23:59:59' "
+        ."AND post_type = 'post' AND post_status = 'publish'"
+    );
+    if ( $ak_post_titles ) {
+        foreach ( (array) $ak_post_titles as $ak_post_title ) {
+
+            /** This filter is documented in wp-includes/post-template.php */
+            $post_title = esc_attr( apply_filters( 'the_title', $ak_post_title->post_title, $ak_post_title->ID ) );
+
+            if ( empty($ak_titles_for_day['day_'.$ak_post_title->dom]) )
+                $ak_titles_for_day['day_'.$ak_post_title->dom] = '';
+            if ( empty($ak_titles_for_day["$ak_post_title->dom"]) ) // first one
+                $ak_titles_for_day["$ak_post_title->dom"] = $post_title;
+            else
+                $ak_titles_for_day["$ak_post_title->dom"] .= $ak_title_separator . $post_title;
+        }
+    }
+
+    // See how much we should pad in the beginning
+    $pad = calendar_week_mod(date('w', $unixmonth)-$week_begins);
+    if ( 0 != $pad )
+        $calendar_output .= "\n\t\t".'<td colspan="'. esc_attr($pad) .'" class="pad">&nbsp;</td>';
+
+    $daysinmonth = intval(date('t', $unixmonth));
+    for ( $day = 1; $day <= $daysinmonth; ++$day ) {
+        if ( isset($newrow) && $newrow )
+            $calendar_output .= "\n\t</tr>\n\t<tr>\n\t\t";
+        $newrow = false;
+
+        if ( $day == gmdate('j', current_time('timestamp')) && $thismonth == gmdate('m', current_time('timestamp')) && $thisyear == gmdate('Y', current_time('timestamp')) )
+            $calendar_output .= '<td id="today">';
+        else
+            $calendar_output .= '<td>';
+
+        if ( in_array($day, $daywithpost) ) // any posts today?
+            $calendar_output .= '<a href="' . get_day_link( $thisyear, $thismonth, $day ) . '" title="' . esc_attr( $ak_titles_for_day[ $day ] ) . "\">$day</a>";
+        else
+            $calendar_output .= $day;
+        $calendar_output .= '</td>';
+
+        if ( 6 == calendar_week_mod(date('w', mktime(0, 0 , 0, $thismonth, $day, $thisyear))-$week_begins) )
+            $newrow = true;
+    }
+
+    $pad = 7 - calendar_week_mod(date('w', mktime(0, 0 , 0, $thismonth, $day, $thisyear))-$week_begins);
+    if ( $pad != 0 && $pad != 7 )
+        $calendar_output .= "\n\t\t".'<td class="pad" colspan="'. esc_attr($pad) .'">&nbsp;</td>';
+
+    $calendar_output .= "\n\t</tr>\n\t</tbody>\n\t</table>";
+
+    $cache[ $key ] = $calendar_output;
+    wp_cache_set( 'aeg_get_calendar', $cache, 'calendar' );
+
+    if ( $echo )
+        echo apply_filters( 'aeg_get_calendar',  $calendar_output );
+    else
+        return apply_filters( 'aeg_get_calendar',  $calendar_output );
 
